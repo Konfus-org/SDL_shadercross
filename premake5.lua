ce71e89784c63a3ab61c0f037338401c3400e2b3
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-08-06 17:46:24 -0700

    Adding premake wrapper around cmake

---
+++ premake5.lua
@@ -0,0 +1,14 @@
+
+local sdlInstall = path.getabsolute("../SDL/cmake/install/" .. os.target() .. "/Release/cmake")
+local cmakeOptions = table.concat({
+    '-DSDL3_DIR="' .. sdlInstall .. '" ',
+    '-DBUILD_SHARED_LIBS=ON',
+    '-DSDLSHADERCROSS_SHARED=ON',
+    '-DSDLSHADERCROSS_STATIC=OFF',
+    '-DSDLSHADERCROSS_INSTALL=ON',
+    '-DSDLSHADERCROSS_VENDORED=ON',
+    '-DCMAKE_CXX_STANDARD=17',
+    '-DCMAKE_CXX_STANDARD_REQUIRED=ON',
+    '-DCMAKE_CXX_EXTENSIONS=OFF'
+}, " ")
+CreateProjectFromCMake("SDL3_Shadercross", "./", cmakeOptions)
